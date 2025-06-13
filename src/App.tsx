5d68e2de25cbaa7b0ef5fd814c195f1f13f4c139
jmcvz <jmchave15360@gmail.com>
2025-06-13 12:44:43 +0800

    JMCC LogIn Module Base Model

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Navbar } from "@/assets/components/pages/Navbar"
+import Login from '@/assets/components/pages/Login';
 /* import { Button } from "@/components/ui/button" */
 
-function App() {
+function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      
       <main className="flex-1 container mx-auto px-4 py-8">
-        {/* Your page content goes here */}
+        {children}
       </main>
     </div>
   );
 }
 
-export default App
+function AppRoutes() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === "/" || location.pathname === "/login";
+
+  return (
+    <>
+      {!hideNavbar ? (
+        <AppLayout>
+          <Routes>
+            {/* Your other routes go here */}
+          </Routes>
+        </AppLayout>
+      ) : (
+        <Routes>
+          {/* Make "/" and "/login" show the login page */}
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      )}
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
